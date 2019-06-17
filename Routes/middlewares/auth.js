8af35e3a080d86e510b44684fa8d081d13c80397
Minh Phan <minhphan2603@gmail.com>
2019-06-17 22:24:58 +0700

    update

---
+++ Routes/middlewares/auth.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+const authenticating = (req,res,next) => { 
+    // verify token
+    //     thanhcong: return next()
+    //     thatbai: res.json(err)
+    const token = req.header('Authorization');
+    
+    try {
+        const decoded = jwt.verify(token,'cybersoft');
+        console.log('giai ma: ',decoded);
+        req.user = decoded;
+        next()
+    } catch (error) {
+        res.status(403).json('khong the login')
+    }
+}
+
+const authorizing = (userTypeArray) => {
+    return (req,res,next) => {
+        const {userType} = req.user
+
+        if (userTypeArray.includes(userType)) {
+            next()
+        }
+        res.status(403).json('ban khong co quyen truy cap')
+    }
+
+}
+
+module.exports = {authenticating , authorizing};
